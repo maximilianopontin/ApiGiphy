ba10afff44ab6ab5dfe028ac33d873750d275a70
maximilianopontin <maxipontin92@gmail.com>
2025-04-21 20:11:16 -0300

    uso de onChange para cambiar valor del inout de formulario

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,24 +5,18 @@ import viteLogo from '/vite.svg'
 
 function App() {
   //hooks para crear estado use state: devuelve una variable que tiene el valor y una funcion que cambia el valor de esa variable (le dice a react que tiene que renderizarse)
-  const [num, setNum] = useState(1); // el usestate entre parentesis tiene el valor inicial de la variable creada, pueden ser numeros, letras, null, etc.
+  const [valorInput, setValorInput] = useState(); // el usestate entre parentesis tiene el valor inicial de la variable creada, pueden ser numeros, letras, null, etc.
 
   //usamos la funcion set de la variable para poder visualizar el cambio en pantalla
-  const sumar = () => {
-    setNum(num + 1);
+  const onChange = (e) => {
+    const valor = e.target.value;//capturamos el valor del evento
+    setValorInput(valor);//lo visualizamos en pantalla
   }
-  const restar = () => {
-    setNum(num - 1);
-  }
+  
   return (
     <>
-      <button onClick={sumar}> 
-        Sumar 1
-      </button>
-      <button onClick= {restar}>
-        Restar 1
-      </button>
-      <p>{num}</p>
+      <input value ={valorInput} onChange={onChange}/> 
+      <p>{valorInput}</p>
     </>
   )
 }
